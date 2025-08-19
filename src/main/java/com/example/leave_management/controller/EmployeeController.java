2f97266c44a98c5874b48a4ef71f626f82cd3abd
chandana <gayamchandana@gmail.com>
2025-08-20 00:02:39 +0530

    Update EmployeeController.java

--- src/main/java/com/example/leave_management/controller/EmployeeController.java
+++ src/main/java/com/example/leave_management/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:3000","https://stirring-tulumba-b45557.netlify.app/"})  // allow frontend React
+@CrossOrigin(origins = "https://stirring-tulumba-b45557.netlify.app")  // allow frontend React
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
